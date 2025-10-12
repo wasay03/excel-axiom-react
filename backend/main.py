d2d066182489ad52ca78cf62cd33786a9802963f
Muhammad Wasay <wasay.arham@gmail.com>
2025-10-12 10:42:56 +0500

    fix

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # IMPORTANT: Allow your frontend (running on localhost:3000) to talk to this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
